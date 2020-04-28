b9f525c8f0549c81071e1e43159ce0076dba3165
Ibrahim Rahmani <ibrahim@xala.no>
2020-04-28 10:20:12 +0200

    Coming soon page.

--- resources/views/kommer.blade.php
+++ resources/views/kommer.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 @section('content')
-<div class="container main-container">
-
-<div class="site-section gray">
-    <div class="container">
-      <div class="title-section mt-4">
-        <h2 class="text-uppercase"><span>KOMMER SNART !</span></h2>
-      </div>
+<div class="site-blocks-cover main_cover" data-aos="fade">
+    <div class="layer">
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 ml-auto order-md-1 align-self-start">
+          <div class="site-block-cover-content">
+          <h1 class="main-title">KOMMER SNART !</h1>
+          <p class="sub-title">VI JOBBER FOR TIDEN MED Å UTVIKLE NETTSIDEN. VI LANSERER SNART.</p>
+          </div>
+        </div>
+        <div class="col-md-6 order-1 align-self-end">
+        </div>
+      </div>
   </div>
 </div>
+
 @endsection
